2e75f012302892afdea74f96ddaa86edbb2a8f0d
StepheninSchool <w0488750@campus.nscc.ca>
2025-01-15 15:56:27 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdultGamingForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
