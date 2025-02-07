720bc7bf226484ae2e3f89b677115c3ea73c156e
StepheninSchool <w0488750@campus.nscc.ca>
2025-02-06 22:27:12 -0400

    Coded comment model and discussion model.

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -2,5 +2,16 @@
 {
     public class Comment
     {
+        //Primary Key
+        public int CommentId { get; set; }
+        //Comment's text content
+        public string Content { get; set; } = string.Empty;
+        //The date and time when the comment was created
+        public DateTime CreateDate { get; set; }
+        //Foreign key to link comment with a discussion
+        public int DiscussionId { get; set; }
+        //Navigation property: The discussion this comment belongs to.
+        public Discussion Discussion { get; set; }
     }
 }
+w
--- Models/Discussion.cs
+++ Models/Discussion.cs
@@ -2,5 +2,15 @@
 {
     public class Discussion
     {
+        public int DiscussionId { get; set; } // Primary Key
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public string ImageFilename { get; set; } = string.Empty;
+
+        public DateTime CreateDate { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
